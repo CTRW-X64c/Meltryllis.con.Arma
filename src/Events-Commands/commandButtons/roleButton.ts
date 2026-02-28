524711e2446533bdad22d618ca59926286ac2ea8
Nowaru <enr_loco_loco@hotmail.com>
2026-02-28 17:46:18 -0600

    ajuste

--- src/Events-Commands/commandButtons/roleButton.ts
+++ src/Events-Commands/commandButtons/roleButton.ts
@@ -17,62 +17,73 @@ export async function registerRoleButtonCommand(): Promise<SlashCommandBuilder[]
     .setDescription("Crea botones para asignar roles en un mensaje. mx 4 botones")
     /* 1er boton */
     .addRoleOption(option => 
-        option.setName("rol")
+        option.setName("first_rol")
         .setDescription("El rol que se dará al aceptar")
         .setRequired(true))
     .addStringOption(option =>
-        option.setName("text_on_button")
+        option.setName("first_text_on_button")
         .setDescription("Texto en el boton")
         .setMaxLength(30)
         .setRequired(true))
     .addStringOption(option =>
-        option.setName("color")
+        option.setName("first_color")
         .setDescription("Color del botón")
         .setRequired(true)
         .addChoices(colorsButton)
     ) /* 2do boton  */
     .addRoleOption(option =>
         option.setName("second_rol")
-        .setDescription("Rol Segundo boton")
+        .setDescription("Rol del 2do boton")
         .setRequired(false))
     .addStringOption(option =>
         option.setName("second_text_on_button")
-        .setDescription("Texto en el boton secundario")
+        .setDescription("Texto del 2do boton")
         .setMaxLength(30)
         .setRequired(false))
     .addStringOption(option =>
         option.setName("second_color")
-        .setDescription("Color del botón secundario")
+        .setDescription("Color del 2do botón")
         .setRequired(false)
         .addChoices(colorsButton)
     ) /* 3ro boton */
     .addRoleOption(option =>
         option.setName("third_rol")
-        .setDescription("Rol Segundo boton")
+        .setDescription("Rol del 3er boton")
         .setRequired(false))
     .addStringOption(option =>
         option.setName("third_text_on_button")
-        .setDescription("Texto en el boton secundario")
+        .setDescription("Texto del 3er boton")
         .setMaxLength(30)
         .setRequired(false))
     .addStringOption(option =>
         option.setName("third_color")
-        .setDescription("Color del botón secundario")
+        .setDescription("Color del 3er botón")
         .setRequired(false)
         .addChoices(colorsButton)
     ) /* 4to boton */
     .addRoleOption(option =>
         option.setName("fourth_rol")
-        .setDescription("Rol Segundo boton")
+        .setDescription("Rol del 4to boton")
         .setRequired(false))
     .addStringOption(option =>
         option.setName("fourth_text_on_button")
-        .setDescription("Texto en el boton secundario")
+        .setDescription("Texto del 4to boton")
         .setMaxLength(30)
         .setRequired(false))
     .addStringOption(option =>
         option.setName("fourth_color")
-        .setDescription("Color del botón secundario")
+        .setDescription("Color del 4to botón")
+        .setRequired(false)
+        .addChoices(colorsButton)
+    ) /* 5to boton */
+    .addStringOption(option =>
+        option.setName("fifth_text_on_button")
+        .setDescription("Texto del 5to boton")
+        .setMaxLength(30)
+        .setRequired(false))
+    .addStringOption(option =>
+        option.setName("fifth_color")
+        .setDescription("Color del 5to botón")
         .setRequired(false)
         .addChoices(colorsButton)
     )
@@ -96,9 +107,9 @@ export async function handleRoleButtonCommand(interaction: ChatInputCommandInter
     }
 
     /* 1er boton */
-    const rol = interaction.options.getRole("rol", true);
-    const color = interaction.options.getString("color", true) as keyof typeof ButtonStyle;
-    const buttonText = interaction.options.getString("text_on_button", true);
+    const firstrol = interaction.options.getRole("first_rol", true);
+    const firstcolor = interaction.options.getString("first_color", true) as keyof typeof ButtonStyle;
+    const firstbuttonText = interaction.options.getString("first_text_on_button", true);
     /* 2do boton */
     const secondRol = interaction.options.getRole("second_rol");
     const secondColor = interaction.options.getString("second_color") as keyof typeof ButtonStyle;
@@ -111,6 +122,10 @@ export async function handleRoleButtonCommand(interaction: ChatInputCommandInter
     const fourthRol = interaction.options.getRole("fourth_rol");
     const fourthColor = interaction.options.getString("fourth_color") as keyof typeof ButtonStyle;
     const fourthButtonText = interaction.options.getString("fourth_text_on_button");
+    /* 5to boton */
+    const fifthRol = interaction.options.getRole("fifth_rol");
+    const fifthColor = interaction.options.getString("fifth_color") as keyof typeof ButtonStyle;
+    const fifthButtonText = interaction.options.getString("fifth_text_on_button");
 
     const channel = interaction.channel as TextChannel;
     await interaction.reply({ 
@@ -124,35 +139,42 @@ export async function handleRoleButtonCommand(interaction: ChatInputCommandInter
         time: 5 * 60 * 1000, 
         max: 1 
     });
-        
+            /* 1er boton */
     collector.on('collect', async (m: Message) => {
         const botonAceptar = new ButtonBuilder()
-        .setCustomId(`roleButton_${rol.id}`)
-        .setLabel(buttonText)
-        .setStyle(ButtonStyle[color]);
+        .setCustomId(`roleButton_${firstrol.id}`)
+        .setLabel(firstbuttonText)
+        .setStyle(ButtonStyle[firstcolor]);
 
     const row = new ActionRowBuilder<ButtonBuilder>().addComponents(botonAceptar);
-
+            /* 2do boton */
         if (secondRol && secondButtonText && secondColor) {
             const secondbutton = new ButtonBuilder()
                 .setCustomId(`roleButton_${secondRol.id}`)
                 .setLabel(secondButtonText)
                 .setStyle(ButtonStyle[secondColor]);
             row.addComponents(secondbutton);
-        }
+        }   /* 3er boton */
         if (thirdRol && thirdButtonText && thirdColor) {
             const thirdbutton = new ButtonBuilder()
                 .setCustomId(`roleButton_${thirdRol.id}`)
                 .setLabel(thirdButtonText)
                 .setStyle(ButtonStyle[thirdColor]);
             row.addComponents(thirdbutton);
-        }
+        }   /* 4to boton */
         if (fourthRol && fourthButtonText && fourthColor) {
             const fourthbutton = new ButtonBuilder()
                 .setCustomId(`roleButton_${fourthRol.id}`)
                 .setLabel(fourthButtonText)
                 .setStyle(ButtonStyle[fourthColor]);
             row.addComponents(fourthbutton);
+        }   /* 5to boton */
+        if (fifthRol && fifthButtonText && fifthColor) {
+            const fifthbutton = new ButtonBuilder()
+                .setCustomId(`roleButton_${fifthRol.id}`)
+                .setLabel(fifthButtonText)
+                .setStyle(ButtonStyle[fifthColor]);
+            row.addComponents(fifthbutton);
         }
         
         const attachments = m.attachments.map(a => a.url);
@@ -186,12 +208,12 @@ export async function roleButton(interaction: ButtonInteraction) {
         try {
             const member = interaction.member as GuildMember;
             if (member.roles.cache.has(roleId)) {
-                await interaction.reply({ content: "❌ Ya habias usado este boton", flags: MessageFlags.Ephemeral });
+                await interaction.reply({ content: `❌ Ya tienes <@&${roleId}>`, flags: MessageFlags.Ephemeral });
                 return;
             }
 
             await member.roles.add(roleId);
-            await interaction.reply({ content: `🎉 ¡Felicidades! Recibiste el rol: <@&${roleId}>.`, flags: MessageFlags.Ephemeral });
+            await interaction.reply({ content: `🎉Recibiste el rol: <@&${roleId}>.`, flags: MessageFlags.Ephemeral });
 
         } catch (error) {
             console.error(`Error al dar el rol desde el botón: ${error}`);
